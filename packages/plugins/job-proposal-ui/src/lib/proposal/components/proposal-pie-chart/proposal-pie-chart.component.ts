2af4080ba0ae6e8d4817454d216f5667da1fabc1
Divyesh <divyeshpythonmate@gmail.com>
2025-10-29 19:39:36 +0530

    Auto-backup: ever-gauzy changes on 2025-10-29 19:39:36 [MOD:       4,ADD:       0,UNT:       0,DEL:       0,REN:       0,TYP:       0,STG:       0 | MOD_FILES:.gitignore,packages/plugins/job-proposal-ui/src/lib/proposal/components/proposal-pie-chart/proposal-pie-chart.component.ts,packages/plugins/job-proposal-ui/src/lib/proposal/resolvers/proposal-details.resolver.ts]

--- packages/plugins/job-proposal-ui/src/lib/proposal/components/proposal-pie-chart/proposal-pie-chart.component.ts
+++ packages/plugins/job-proposal-ui/src/lib/proposal/components/proposal-pie-chart/proposal-pie-chart.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { NbJSThemeVariable, NbThemeService } from '@nebular/theme';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslationBaseComponent } from '@gauzy/ui-core/i18n';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 
 // Define the type for JSThemeVariable
 type JSThemeVariable = string | NbJSThemeVariable | string[];
@@ -10,11 +11,17 @@ type JSThemeVariable = string | NbJSThemeVariable | string[];
 @Component({
 	standalone: false,
 	selector: 'ngx-proposal-pie-chart',
-	template: `<canvas baseChart [options]="options" class="echart"></canvas>`,
+	template: `<canvas baseChart [data]="chartData" [options]="chartOptions" [type]="chartType" class="echart"></canvas>`,
 	styleUrls: ['./proposal-pie-chart.component.scss']
 })
 export class ProposalPieChartComponent extends TranslationBaseComponent implements AfterViewInit, OnDestroy {
-	public options: any = {};
+	public chartData: ChartData<'pie'> = {
+		labels: [],
+		datasets: []
+	};
+	
+	public chartOptions: ChartConfiguration['options'] = {};
+	public chartType: ChartType = 'pie';
 	private subscription: Subscription;
 
 	@Input() values: { name: string; value: number }[];
@@ -27,8 +34,27 @@ export class ProposalPieChartComponent extends TranslationBaseComponent implemen
 		this.subscription = this.nbThemeService.getJsTheme().subscribe((config) => {
 			const { warningLight, infoLight, dangerLight, successLight, primaryLight, echarts } = config.variables;
 
+			// Set the chart data
+			this.chartData = {
+				labels: [
+					this.getTranslation('PROPOSALS_PAGE.ACCEPTED_PROPOSALS'),
+					this.getTranslation('PROPOSALS_PAGE.TOTAL_PROPOSALS')
+				],
+				datasets: [{
+					data: this.values?.map(item => item.value) || [],
+					backgroundColor: [
+						this.getColorString(warningLight),
+						this.getColorString(infoLight),
+						this.getColorString(dangerLight),
+						this.getColorString(successLight),
+						this.getColorString(primaryLight)
+					] as any,
+					label: this.getTranslation('PROPOSALS_PAGE.PROPOSALS')
+				}]
+			};
+
 			// Set the chart options
-			this.options = this.getChartOptions(
+			this.chartOptions = this.getChartOptions(
 				warningLight,
 				infoLight,
 				dangerLight,
@@ -57,59 +83,63 @@ export class ProposalPieChartComponent extends TranslationBaseComponent implemen
 		successLight: JSThemeVariable,
 		primaryLight: JSThemeVariable,
 		echarts: any
-	) {
+	): ChartConfiguration['options'] {
 		return {
+			responsive: true,
 			backgroundColor: echarts.bg,
-			color: [warningLight, infoLight, dangerLight, successLight, primaryLight],
-			tooltip: {
-				trigger: 'item',
-				formatter: '{a} <br/>{b} : {c} ({d}%)'
-			},
-			legend: {
-				orient: 'vertical',
-				left: 'left',
-				data: [
-					this.getTranslation('PROPOSALS_PAGE.ACCEPTED_PROPOSALS'),
-					this.getTranslation('PROPOSALS_PAGE.TOTAL_PROPOSALS')
-				],
-				textStyle: {
-					color: echarts.textColor
-				}
-			},
-			series: [
-				{
-					name: this.getTranslation('PROPOSALS_PAGE.PROPOSALS'),
-					type: 'pie',
-					radius: '80%',
-					center: ['50%', '50%'],
-					data: this.values,
-					itemStyle: {
-						emphasis: {
-							shadowBlur: 10,
-							shadowOffsetX: 0,
-							shadowColor: echarts.itemHoverShadowColor
-						}
-					},
-					label: {
-						normal: {
-							textStyle: {
-								color: echarts.textColor
-							}
-						}
-					},
-					labelLine: {
-						normal: {
-							lineStyle: {
-								color: echarts.axisLineColor
-							}
+			plugins: {
+				legend: {
+					display: true,
+					position: 'left',
+					labels: {
+						color: echarts.textColor
+					}
+				},
+				tooltip: {
+					enabled: true,
+					callbacks: {
+						label: (context) => {
+							return `${context.label}: ${context.parsed}`;
 						}
 					}
 				}
-			]
+			}
 		};
 	}
 
 	ngOnDestroy(): void {
 		this.subscription?.unsubscribe();
+	}
+
+	/**
+	 * Converts a theme variable to a string color value
+	 * 
+	 * @param color The theme variable color
+	 * @returns The string representation of the color
+	 */
+	private getColorString(color: JSThemeVariable): string {
+		// If it's already a string, return it directly
+		if (typeof color === 'string') {
+			return color;
+		}
+		
+		// If it's an array, return the first element if it exists
+		if (Array.isArray(color)) {
+			return color.length > 0 ? color[0] : '#000000';
+		}
+		
+		// For NbJSThemeVariable objects, try to extract a color value
+		if (color && typeof color === 'object') {
+			// Try common properties that might contain color values
+			if ('color' in color) {
+				return this.getColorString(color.color);
+			}
+			if ('background' in color) {
+				return this.getColorString(color.background);
+			}
+		}
+		
+		// Default fallback
+		return '#000000';
 	}
 }
